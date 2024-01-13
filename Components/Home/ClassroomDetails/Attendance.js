d7a9241600fac829c00461a352115cc2d5c494db
Akshay Kubal <akshay2298060@yahoo.co.in>
2024-01-13 19:53:02 +0530

    Attendance ProgressCircle

--- Components/Home/ClassroomDetails/Attendance.js
+++ Components/Home/ClassroomDetails/Attendance.js
@@ -1,12 +1,40 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Progress from "react-native-progress";
 
 const Attendance = () => {
+  const [attendance, setAttendance] = useState(0);
+
+  useEffect(() => {
+    setAttendance(0.6);
+  }, []);
+
   return (
-    <View style={{ alignItems: "center" }}>
-      <Text style={{ color: "white" }}>Attendance</Text>
+    <View
+      style={{
+        alignItems: "center",
+        alignContent: "center",
+        flex: 1,
+        justifyContent: "center",
+      }}
+    >
+      <View style={styles.progressCircle}>
+        <Progress.Circle
+          progress={attendance}
+          size={110}
+          borderWidth={0}
+          color="white"
+          thickness={6}
+          strokeCap="round"
+          showsText
+        />
+      </View>
     </View>
   );
 };
 
 export default Attendance;
+
+const styles = StyleSheet.create({
+  progressCircle: {},
+});
